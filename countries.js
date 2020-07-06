4cf986f916992fbddf5a11086e7c83cd3905dce4
Tomasz Latusek <tomaszlatusek.tl@gmail.com>
2020-07-06 12:55:08 +0200

    First draft

---
+++ countries.js
@@ -0,0 +1,8 @@
+const COUNTRIES = [
+    {name = "Albania", capital = "Tirana",  population = 3038594, ue = false},
+    {name = "Andora", capital = "Andora", population = 78014, ue = false},
+    {name = "Austria", capital = "Vienna", population = 8859992, ue = true},
+]
+
+
+
